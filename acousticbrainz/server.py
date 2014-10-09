3ac90fdea3f5e3c84cdaea97fa9f9143bd296a73
Robert Kaye <rob@musicbrainz.org>
2014-10-09 13:17:07 +0200

    Implemented and debugged the low level end points

--- acousticbrainz/server.py
+++ acousticbrainz/server.py
@@ -3,10 +3,10 @@ import json
 import psycopg2
 import logging
 import config
-from utils import validate_uuid
+import uuid
 from logging.handlers import RotatingFileHandler
-from flask import Flask, request, Response
-from werkzeug.exceptions import BadRequest, ServiceUnavailable
+from flask import Flask, request, Response, jsonify
+from werkzeug.exceptions import BadRequest, ServiceUnavailable, NotFound, InternalServerError
 
 app = Flask(__name__)
 
@@ -18,38 +18,50 @@ handler = RotatingFileHandler(config.LOG_FILE)
 handler.setLevel(logging.WARNING)
 app.logger.addHandler(handler)
 
+def validate_uuid(string, version=4):
+    """Validates UUID of a specified version (default version is 4).
+
+    Returns:
+        True if UUID is valid.
+        False otherwise.
+    """
+    try:
+        _ = uuid.UUID(string, version=version)
+    except ValueError:
+        return False
+    return True
+
 @app.route("/")
 def index():
     return "<html>Piss off!</html>"
 
 @app.route("/<mbid>/low-level", methods=["POST"])
-def submit_low_leval(mbid):
+def submit_low_level(mbid):
     """Endpoint for submitting low-level information to AcousticBrainz"""
 
+    raw_data = request.get_data()
     try:
-        metadata, data = request.get_data.split("\n")
-    except ValueError:
-        raise BadRequest("Two JSON documents are required in the body of the POST request.")
-
-    try:
-        metadata = json.loads(metadata)
+        data = json.loads(raw_data)
     except ValueError, e:
-        raise BadRequest("Cannot parse metadata JSON document: %s" % e)
-
-    is_lossless_submit = metadata['lossless']
-    build_sha1 = metadata['build_sha1']
+        raise BadRequest("Cannot parse JSON document: %s" % e)
 
     try:
-        data = json.loads(data)
-    except ValueError, e:
-        raise BadRequest("Cannot parse data JSON document: %s" % e)
+        is_lossless_submit = data['metadata']['audio_properties']['lossless']
+        build_sha1 = data['metadata']['version']['essentia_build_sha']
+    except KeyError:
+        raise BadRequest("Submitted JSON document does not seem to be Essentia output.")
 
     if not validate_uuid(mbid):
         raise BadRequest("Invalid MBID: %s" % e)
 
+    # if the user submitted a trackid key, rewrite to recording_id
+    if data['metadata']['tags'].has_key("musicbrainz_trackid"):
+        val = data['metadata']['tags']["musicbrainz_trackid"]
+        del data['metadata']['tags']["musicbrainz_trackid"]
+        data['metadata']['tags']["musicbrainz_recordingid"] = val
+
     conn = psycopg2.connect(config.PG_CONNECT)
     cur = conn.cursor()
-
     try:
         # Checking to see if we already have this data
         cur.execute("SELECT lossless FROM lowlevel WHERE mbid = %s", (mbid, ))
@@ -58,16 +70,18 @@ def submit_low_leval(mbid):
         if not cur.rowcount:
             cur.execute("INSERT INTO lowlevel (mbid, build_sha1, lossless, data)"
                         "VALUES (%s, %s, %s, %s)",
-                        (mbid, build_sha1, is_lossless_submit, data))
+                        (mbid, build_sha1, is_lossless_submit, json.dumps(data)))
             conn.commit()
+            return ""
 
         # if we have a lossy version and this submission is a lossless one, replace it
         row = cur.fetchone()
         if is_lossless_submit and not row[0]:
             cur.execute("UPDATE lowlevel (mbid, build_sha1, lossless, data, submitted)"
                         "VALUES (%s, %s, %s, %s, now())",
-                        (mbid, build_sha1, is_lossless_submit, data))
+                        (mbid, build_sha1, is_lossless_submit, json.dumps(data)))
             conn.commit()
+            return ""
 
     except psycopg2.IntegrityError, e:
         raise BadRequest(str(e))
@@ -76,5 +90,29 @@ def submit_low_leval(mbid):
 
     return ""
 
+@app.route("/<mbid>/low-level", methods=["GET"])
+def get_low_level(mbid):
+    """Endpoint for fetching low-level information to AcousticBrainz"""
+
+    if not validate_uuid(mbid):
+        raise BadRequest("Invalid MBID: %s" % e)
+
+    conn = psycopg2.connect(config.PG_CONNECT)
+    cur = conn.cursor()
+    try:
+        cur.execute("SELECT data::text FROM lowlevel WHERE mbid = %s", (mbid, ))
+        if not cur.rowcount:
+            raise NotFound
+
+        row = cur.fetchone()
+        return Response(row[0], content_type='application/json')
+
+    except psycopg2.IntegrityError, e:
+        raise BadRequest(str(e))
+    except psycopg2.OperationalError, e:
+        raise ServiceUnavailable(str(e))
+
+    return InternalServerError("whoops, looks like a cock-up on our part!")
+
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8080)
+    app.run(debug=True, host="127.0.0.1", port=8080)

aeed95928b325f0f6d1da280a97b6bf223dd79e7
Robert Kaye <rob@musicbrainz.org>
2014-11-14 06:26:40 -0800

    More bits of tweaking the dumps

--- admin/dump-json.py
+++ admin/dump-json.py
@@ -12,7 +12,7 @@ import config
 from datetime import datetime
 from time import gmtime, strftime
 
-DUMP_CHUNK_SIZE = 2
+DUMP_CHUNK_SIZE = 1000
 
 def create_path(path):
     """Creates a directory structure if it doesn't exist yet."""
@@ -68,14 +68,12 @@ def dump_lowlevel_json(location=os.path.join(os.getcwd(), 'dump'), rotate=False)
     with tarfile.open("%s/acousticbrainz-lowlevel-json-%s-json.tar.bz2" % (location, datetime.today().strftime('%Y%m%d')), "w:bz2") as tar:
         last_mbid = ""
         index = 0
-        count = 0
         print "  run queries..."
         cur.execute("""SELECT id FROM lowlevel ll ORDER BY mbid""")
         while True:
             id_list = cur.fetchmany(size = DUMP_CHUNK_SIZE)
             if not id_list:
                 break
-
 
             id_list = tuple([ i[0] for i in id_list ])
 
@@ -110,7 +108,6 @@ def dump_lowlevel_json(location=os.path.join(os.getcwd(), 'dump'), rotate=False)
                 count += 1
 
 
-
         # Copying legal text
         tar.add("../licenses/COPYING-PublicDomain", arcname=os.path.join("acousticbrainz-lowlevel-json-" + datetime.today().strftime('%Y%m%d'), 'COPYING'))
 
@@ -132,40 +129,55 @@ def dump_highlevel_json(location=os.path.join(os.getcwd(), 'dump'), rotate=False
     create_path(temp_dir)
 
     conn = psycopg2.connect(config.PG_CONNECT)
+    conn2 = psycopg2.connect(config.PG_CONNECT)
     cur = conn.cursor()
+    cur2 = conn.cursor()
 
     with tarfile.open("%s/acousticbrainz-highlevel-json-%s-json.tar.bz2" % (location, datetime.today().strftime('%Y%m%d')), "w:bz2") as tar:
         last_mbid = ""
         index = 0
-        count = 0
-        print "  run query.."
-        cur.execute("""SELECT mbid, hlj.data::text FROM highlevel hl, highlevel_json hlj WHERE hl.data = hlj.id ORDER BY mbid""")
+        print "  run queries..."
+        cur.execute("""SELECT hl.id FROM highlevel hl, highlevel_json hlj WHERE hl.data = hlj.id ORDER BY mbid""")
         while True:
-            row = cur.fetchone()
-            if not row:
+            id_list = cur.fetchmany(size = DUMP_CHUNK_SIZE)
+            if not id_list:
                 break
 
-            mbid = row[0]
-            json = row[1]
+            id_list = tuple([ i[0] for i in id_list ])
 
-            if mbid == last_mbid:
-                index += 1
-            else:
-                index = 0
+            count = 0
+            cur2.execute("""SELECT mbid, hlj.data::text 
+                             FROM highlevel hl, highlevel_json hlj 
+                            WHERE hl.data = hlj.id 
+                              AND hl.id IN %s
+                            ORDER BY mbid""", (id_list, ))
+            while True:
+                row = cur2.fetchone()
+                if not row:
+                    break
 
-            filename = os.path.join(location, mbid + "-%d.json" % index)
-            if count % 1000 == 0:
-                print "\r  write %s" % filename,
-            f = open(filename, "w")
-            f.write(json)
-            f.close()
-       
-            arcname = os.path.join("acousticbrainz-highlevel-json-" + datetime.today().strftime('%Y%m%d'), "highlevel", mbid[0:1], mbid[0:2], mbid + "-%d.json" % index)
-            tar.add(filename, arcname=arcname)
-            os.unlink(filename)
+                mbid = row[0]
+                json = row[1]
 
-            last_mbid = mbid
-            count += 1
+                if count == 0:
+                    print "\r  write %s" % mbid,
+
+                if mbid == last_mbid:
+                    index += 1
+                else:
+                    index = 0
+
+                filename = os.path.join(location, mbid + "-%d.json" % index)
+                f = open(filename, "w")
+                f.write(json)
+                f.close()
+           
+                arcname = os.path.join("acousticbrainz-highlevel-json-" + datetime.today().strftime('%Y%m%d'), "highlevel", mbid[0:1], mbid[0:2], mbid + "-%d.json" % index)
+                tar.add(filename, arcname=arcname)
+                os.unlink(filename)
+
+                last_mbid = mbid
+                count += 1
 
 
         # Copying legal text

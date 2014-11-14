f41750d5b54f65e0e57069d51f22f83d652c2221
Robert Kaye <rob@musicbrainz.org>
2014-11-14 05:55:18 -0800

    Split the low level dump query so that it finishes in a reasonable amount of time

--- admin/dump-json.py
+++ admin/dump-json.py
@@ -12,6 +12,8 @@ import config
 from datetime import datetime
 from time import gmtime, strftime
 
+DUMP_CHUNK_SIZE = 2
+
 def create_path(path):
     """Creates a directory structure if it doesn't exist yet."""
     try:
@@ -59,40 +61,50 @@ def dump_lowlevel_json(location=os.path.join(os.getcwd(), 'dump'), rotate=False)
     create_path(temp_dir)
 
     conn = psycopg2.connect(config.PG_CONNECT)
+    conn2 = psycopg2.connect(config.PG_CONNECT)
     cur = conn.cursor()
+    cur2 = conn.cursor()
 
     with tarfile.open("%s/acousticbrainz-lowlevel-json-%s-json.tar.bz2" % (location, datetime.today().strftime('%Y%m%d')), "w:bz2") as tar:
         last_mbid = ""
         index = 0
         count = 0
         print "  run query.."
-        cur.execute("""SELECT mbid, data::text FROM lowlevel ll ORDER BY mbid""")
+        cur.execute("""SELECT id FROM lowlevel ll ORDER BY mbid""")
         while True:
-            row = cur.fetchone()
-            if not row:
+            id_list = cur.fetchmany(size = DUMP_CHUNK_SIZE)
+            if not id_list:
                 break
 
-            mbid = row[0]
-            json = row[1]
+            id_list = tuple([ i[0] for i in id_list ])
 
-            if mbid == last_mbid:
-                index += 1
-            else:
-                index = 0
+            cur2.execute("SELECT mbid, data::text FROM lowlevel WHERE id IN %s ORDER BY mbid", (id_list,))
+            while True:
+                row = cur2.fetchone()
+                if not row:
+                    break
 
-            filename = os.path.join(location, mbid + "-%d.json" % index)
-            if count % 1000 == 0:
-                print "\r  write %s" % filename,
-            f = open(filename, "w")
-            f.write(json)
-            f.close()
-       
-            arcname = os.path.join("acousticbrainz-lowlevel-json-" + datetime.today().strftime('%Y%m%d'), "lowlevel", mbid[0:1], mbid[0:2], mbid + "-%d.json" % index)
-            tar.add(filename, arcname=arcname)
-            os.unlink(filename)
+                mbid = row[0]
+                json = row[1]
 
-            last_mbid = mbid
-            count += 1
+                if mbid == last_mbid:
+                    index += 1
+                else:
+                    index = 0
+
+                filename = os.path.join(location, mbid + "-%d.json" % index)
+                f = open(filename, "w")
+                f.write(json)
+                f.close()
+           
+                arcname = os.path.join("acousticbrainz-lowlevel-json-" + datetime.today().strftime('%Y%m%d'), "lowlevel", mbid[0:1], mbid[0:2], mbid + "-%d.json" % index)
+                tar.add(filename, arcname=arcname)
+                os.unlink(filename)
+
+                last_mbid = mbid
+                count += 1
+
+            print "\r  write %s" % last_mbid,
 
 
         # Copying legal text

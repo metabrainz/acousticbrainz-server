676eaf437db2e67e8eb2d00fe111fea5af198291
Robert Kaye <rob@musicbrainz.org>
2014-11-22 15:33:52 +0100

    Committing my speed improvements to the cleanup script that we shouldn't need anylonger

--- admin/clean-tags.py
+++ admin/clean-tags.py
@@ -19,6 +19,7 @@ def rewrite_lowlevel():
     cur3 = conn3.cursor()
 
     cur.execute("""SELECT id FROM lowlevel ll ORDER BY mbid""")
+    total = 0
     while True:
         id_list = cur.fetchmany(size = DUMP_CHUNK_SIZE)
         if not id_list:
@@ -26,8 +27,8 @@ def rewrite_lowlevel():
 
         id_list = tuple([ i[0] for i in id_list ])
 
-        count = 0
         cur2.execute("SELECT id, mbid, data FROM lowlevel WHERE id IN %s ORDER BY mbid", (id_list,))
+        count = 0
         while True:
             row = cur2.fetchone()
             if not row:
@@ -37,21 +38,31 @@ def rewrite_lowlevel():
             mbid = row[1]
             data = row[2]
             # print mbid
-            tags = data["metadata"]["tags"]
+            try:
+                tags = data["metadata"]["tags"]
+            except KeyError:
+                continue
+
             filename = tags["file_name"]
+            dirty = False
             for k in tags.keys():
                 k = k.lower()
-                if k.find('lyric') >= 0 or k.find('comment') >= 0:
+                if k.find('lyrics') >= 0 or k.find('comment') >= 0:
                     del tags[k]
-            tags["file_name"] = filename
-            data["metadata"]["tags"] = tags
-            query = "UPDATE lowlevel SET data = %s, data_sha256 = %s WHERE id = %s"
-            data_json = json.dumps(data, sort_keys=True, separators=(',', ':'))
-            data_sha256 = sha256(data_json).hexdigest()
+                    dirty = True
 
-            cur3.execute(query, (data_json, data_sha256, id))
-            conn3.commit()
+            if dirty:
+                tags["file_name"] = filename
+                data["metadata"]["tags"] = tags
+                query = "UPDATE lowlevel SET data = %s, data_sha256 = %s WHERE id = %s"
+                data_json = json.dumps(data, sort_keys=True, separators=(',', ':'))
+                data_sha256 = sha256(data_json).hexdigest()
 
+                cur3.execute(query, (data_json, data_sha256, id))
+                conn3.commit()
+                count += 1
+            total += 1
+        print "%d: %d entries cleaned" % (total, count)
 
 def rewrite_highlevel():
     conn = psycopg2.connect(config.PG_CONNECT)
@@ -62,6 +73,8 @@ def rewrite_highlevel():
     cur3 = conn3.cursor()
 
     cur.execute("""SELECT id FROM highlevel_json ORDER BY id""")
+    count = 0
+    total = 0
     while True:
         id_list = cur.fetchmany(size = DUMP_CHUNK_SIZE)
         if not id_list:
@@ -79,24 +92,34 @@ def rewrite_highlevel():
             id = row[0]
             data = row[1]
             # print mbid
-            tags = data["metadata"]["tags"]
+            try:
+                tags = data["metadata"]["tags"]
+            except KeyError:
+                continue
+
             filename = tags["file_name"]
+            dirty = False
             for k in tags.keys():
                 k = k.lower()
-                if k.find('lyric') >= 0 or k.find('comment') >= 0:
-                    print "Stripping tag %s" % k
+                if k.find('lyrics') >= 0 or k.find('comment') >= 0:
                     del tags[k]
-            tags["file_name"] = filename
-            data["metadata"]["tags"] = tags
-            query = "UPDATE highlevel_json SET data = %s, data_sha256 = %s WHERE id = %s"
-            data_json = json.dumps(data, sort_keys=True, separators=(',', ':'))
-            data_sha256 = sha256(data_json).hexdigest()
+                    dirty = True
 
-            cur3.execute(query, (data_json, data_sha256, id))
-            conn3.commit()
+            if dirty:
+                tags["file_name"] = filename
+                data["metadata"]["tags"] = tags
+                query = "UPDATE highlevel_json SET data = %s, data_sha256 = %s WHERE id = %s"
+                data_json = json.dumps(data, sort_keys=True, separators=(',', ':'))
+                data_sha256 = sha256(data_json).hexdigest()
+
+                cur3.execute(query, (data_json, data_sha256, id))
+                conn3.commit()
+                count += 1
+            total += 1
+        print "%d: %d entries cleaned" % (total, count)
 
 
 # Go through the database rewriting metadata blocks to
 if __name__ == "__main__":
-    #rewrite_lowlevel()
+    rewrite_lowlevel()
     rewrite_highlevel()

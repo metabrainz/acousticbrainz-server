0c69dc2aa302ca4aef3262e1840721e41855b472
Roman Tsukanov <roman@tsukanov.me>
2014-12-13 06:32:07 +0900

    Added basic tests for the rest of index views.

--- acousticbrainz/views/index_test.py
+++ acousticbrainz/views/index_test.py
@@ -7,3 +7,19 @@ class IndexViewsTestCase(FlaskTestCase):
     def test_index(self):
         resp = self.client.get(url_for('index.index'))
         self.assert200(resp)
+
+    def test_download(self):
+        resp = self.client.get(url_for('index.download'))
+        self.assert200(resp)
+
+    def test_contribute(self):
+        resp = self.client.get(url_for('index.contribute'))
+        self.assert200(resp)
+
+    def test_goals(self):
+        resp = self.client.get(url_for('index.goals'))
+        self.assert200(resp)
+
+    def test_faq(self):
+        resp = self.client.get(url_for('index.faq'))
+        self.assert200(resp)

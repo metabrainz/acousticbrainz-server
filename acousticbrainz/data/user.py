417b4583bff8138c50b42f44a26971faa8df6f26
Roman Tsukanov <roman@tsukanov.me>
2015-02-06 09:03:03 +0900

    Catching DB exceptions in data.user module.

--- acousticbrainz/data/user.py
+++ acousticbrainz/data/user.py
@@ -1,26 +1,40 @@
 import psycopg2
 from flask import current_app
 from flask_login import UserMixin
+from werkzeug.exceptions import BadRequest, ServiceUnavailable
 
 
 def create(musicbrainz_id):
-    connection = psycopg2.connect(current_app.config['PG_CONNECT'])
-    cursor = connection.cursor()
-    cursor.execute('INSERT INTO "user" (musicbrainz_id) VALUES (%s)',
-                   (str(musicbrainz_id),))
-    connection.commit()
+    try:
+        connection = psycopg2.connect(current_app.config['PG_CONNECT'])
+        cursor = connection.cursor()
+        cursor.execute('INSERT INTO "user" (musicbrainz_id) VALUES (%s)',
+                       (str(musicbrainz_id),))
+        connection.commit()
 
-    # Getting new user's ID
-    cursor.execute('SELECT id FROM "user" WHERE musicbrainz_id = %s',
-                   (str(musicbrainz_id),))
+        # Getting new user's ID
+        cursor.execute('SELECT id FROM "user" WHERE musicbrainz_id = %s',
+                       (str(musicbrainz_id),))
+    except psycopg2.ProgrammingError, e:
+        raise BadRequest(str(e))
+    except psycopg2.IntegrityError, e:
+        raise BadRequest(str(e))
+    except psycopg2.OperationalError, e:
+        raise ServiceUnavailable(str(e))
+
     return cursor.fetchone()[0]
 
 
 def get(id):
-    connection = psycopg2.connect(current_app.config['PG_CONNECT'])
-    cursor = connection.cursor()
-    cursor.execute('SELECT id, created, musicbrainz_id FROM "user" WHERE id = %s',
-                   (str(id),))
+    try:
+        connection = psycopg2.connect(current_app.config['PG_CONNECT'])
+        cursor = connection.cursor()
+        cursor.execute('SELECT id, created, musicbrainz_id FROM "user" WHERE id = %s',
+                       (str(id),))
+    except psycopg2.IntegrityError, e:
+        raise BadRequest(str(e))
+    except psycopg2.OperationalError, e:
+        raise ServiceUnavailable(str(e))
 
     if cursor.rowcount > 0:
         row = cursor.fetchone()
@@ -34,10 +48,15 @@ def get(id):
 
 
 def get_by_mb_id(musicbrainz_id):
-    connection = psycopg2.connect(current_app.config['PG_CONNECT'])
-    cursor = connection.cursor()
-    cursor.execute('SELECT id, created FROM "user" WHERE musicbrainz_id = %s',
-                   (str(musicbrainz_id),))
+    try:
+        connection = psycopg2.connect(current_app.config['PG_CONNECT'])
+        cursor = connection.cursor()
+        cursor.execute('SELECT id, created FROM "user" WHERE musicbrainz_id = %s',
+                       (str(musicbrainz_id),))
+    except psycopg2.IntegrityError, e:
+        raise BadRequest(str(e))
+    except psycopg2.OperationalError, e:
+        raise ServiceUnavailable(str(e))
 
     if cursor.rowcount > 0:
         row = cursor.fetchone()

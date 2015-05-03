e08715d868f331bfcc1914e68e89c2435da1dfd7
Roman Tsukanov <roman@tsukanov.me>
2015-05-03 10:48:16 +0900

    Added cache flushing into deployment process in fabfile.

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,7 @@
 from __future__ import with_statement
 from fabric.api import local
 from fabric.colors import green
+from acousticbrainz import create_app, cache
 
 
 def compile_styling():
@@ -13,5 +14,12 @@ def compile_styling():
     print(green("Style sheets have been compiled successfully.", bold=True))
 
 
+def clear_memcached():
+    with create_app().app_context():
+        cache.flush_all()
+    print(green("Flushed everything from memcached.", bold=True))
+
+
 def deploy():
     compile_styling()
+    clear_memcached()

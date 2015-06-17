63f196fb0bf5581e2c354260efdafd8b682f82cc
Roman Tsukanov <roman@tsukanov.me>
2015-06-18 06:16:20 +0900

    Added tests for data.dataset module.

---
+++ acousticbrainz/data/dataset_test.py
@@ -0,0 +1,21 @@
+from acousticbrainz.testing import ServerTestCase
+from acousticbrainz.data import dataset, user
+
+
+class DatasetTestCase(ServerTestCase):
+
+    def setUp(self):
+        super(DatasetTestCase, self).setUp()
+
+        self.test_user_mb_name = "tester"
+        self.test_user_id = user.create(self.test_user_mb_name)
+
+        self.test_data = {
+            "name": "Test",
+            "description": "",
+            "classes": [],
+        }
+
+    def test_create_from_dict(self):
+        id = dataset.create_from_dict(self.test_data, author_id=self.test_user_id)
+        self.assertIsNotNone(dataset.get(id))

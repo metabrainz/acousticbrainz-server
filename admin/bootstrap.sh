3222d189895da6dce82fe4ddda45fbf0c17f704d
Roman Tsukanov <roman@tsukanov.me>
2015-06-27 16:08:36 +0900

    Split up database setup scripts from web server stuff.

--- admin/bootstrap.sh
+++ admin/bootstrap.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
+# Application setup script for Vagrant
 
 apt-get update
 apt-get -y upgrade
 
+apt-get -y install python-virtualenv python-dev pxz
+
 cd /vagrant
+./admin/install_database.sh /vagrant
 ./admin/install_web_server.sh /vagrant
 ./admin/install_hl_extractor.sh /vagrant

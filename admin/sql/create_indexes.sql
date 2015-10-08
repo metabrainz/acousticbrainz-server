0ca531e744abee30f6c61f19934f331135a0e46d
Alastair Porter <alastair@porter.net.nz>
2015-10-08 17:36:46 +0200

    Make the lower username index unique

--- admin/sql/create_indexes.sql
+++ admin/sql/create_indexes.sql
@@ -7,6 +7,6 @@ CREATE UNIQUE INDEX data_sha256_ndx_lowlevel ON lowlevel (data_sha256);
 CREATE INDEX mbid_ndx_highlevel ON highlevel (mbid);
 CREATE INDEX build_sha1_ndx_highlevel ON lowlevel (build_sha1);
 
-CREATE INDEX lower_musicbrainz_id_ndx_user ON "user" (lower(musicbrainz_id));
+CREATE UNIQUE INDEX lower_musicbrainz_id_ndx_user ON "user" (lower(musicbrainz_id));
 
 COMMIT;
--- admin/updates/update-9.sql
+++ admin/updates/update-9.sql
@@ -1,5 +1,5 @@
 BEGIN;
 
-CREATE INDEX lower_musicbrainz_id_ndx_user ON "user" (lower(musicbrainz_id));
+CREATE UNIQUE INDEX lower_musicbrainz_id_ndx_user ON "user" (lower(musicbrainz_id));
 
 COMMIT;

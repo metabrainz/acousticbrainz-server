5a15b2ef313a5c8431bc4677588293c29baa769c
Alastair Porter <alastair@porter.net.nz>
2015-10-20 20:00:46 +0200

    Make highlevel_meta data_sha256 index non-unique

--- admin/sql/create_indexes.sql
+++ admin/sql/create_indexes.sql
@@ -8,10 +8,12 @@ CREATE UNIQUE INDEX data_sha256_ndx_lowlevel_json ON lowlevel_json (data_sha256)
 CREATE INDEX mbid_ndx_highlevel ON highlevel (mbid);
 CREATE INDEX build_sha1_ndx_highlevel ON highlevel (build_sha1);
 
-CREATE UNIQUE INDEX data_sha256_ndx_highlevel_meta ON highlevel_meta (data_sha256);
+-- This should not be unique because the exact same file could be submitted twice
+-- and for now we should just store the meta block twice
+CREATE INDEX data_sha256_ndx_highlevel_meta ON highlevel_meta (data_sha256);
 -- This index should not be unique on data, because it's possible to have the same estimate for
 -- two different recordings. Instead map it over data, model, and the id of the item
-CREATE INDEX data_sha256_ndx_highlevel_model ON highlevel_model (data_sha256, highlevel, model);
+CREATE UNIQUE INDEX data_sha256_ndx_highlevel_model ON highlevel_model (data_sha256, highlevel, model);
 CREATE INDEX model_ndx_highlevel_model ON highlevel_model (model);
 
 CREATE UNIQUE INDEX lower_musicbrainz_id_ndx_user ON "user" (lower(musicbrainz_id));

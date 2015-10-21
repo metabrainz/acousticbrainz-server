3f59d6fe11564d6776ebfe13e8ad455f86ad97d0
Roman Tsukanov <roman@tsukanov.me>
2015-10-21 22:55:48 +0200

    Fix database initialization.

--- admin/sql/create_db.sql
+++ admin/sql/create_db.sql
@@ -2,5 +2,5 @@
 
 -- Create the user and the database. Must run as user postgres.
 
--- CREATE USER acousticbrainz NOCREATEDB NOCREATEUSER;
+CREATE USER acousticbrainz NOCREATEDB NOCREATEUSER;
 CREATE DATABASE acousticbrainz94 WITH OWNER = acousticbrainz TEMPLATE template0 ENCODING = 'UNICODE';

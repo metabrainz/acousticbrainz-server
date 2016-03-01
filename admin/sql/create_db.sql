c4357720eee55926ca760191d0050664d5ce3f60
Alastair Porter <alastair@porter.net.nz>
2016-03-01 22:01:56 +0100

    Remove "version" from database name

--- admin/sql/create_db.sql
+++ admin/sql/create_db.sql
@@ -3,4 +3,4 @@
 -- Create the user and the database. Must run as user postgres.
 
 CREATE USER acousticbrainz NOCREATEDB NOCREATEUSER;
-CREATE DATABASE acousticbrainz94 WITH OWNER = acousticbrainz TEMPLATE template0 ENCODING = 'UNICODE';
+CREATE DATABASE acousticbrainz WITH OWNER = acousticbrainz TEMPLATE template0 ENCODING = 'UNICODE';
--- manage.py
+++ manage.py
@@ -62,7 +62,7 @@ def init_db(archive, force):
         raise Exception('Failed to create new database and user! Exit code: %i' % exit_code)
 
     print('Creating database extensions...')
-    exit_code = _run_psql('create_extensions.sql', 'acousticbrainz94')
+    exit_code = _run_psql('create_extensions.sql', 'acousticbrainz')
     if exit_code != 0:
         raise Exception('Failed to create database extensions! Exit code: %i' % exit_code)
 

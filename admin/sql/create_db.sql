48679d68eaa176155026dc6aa421ad5422f5e5f7
Param Singh <param@metabrainz.org>
2017-11-15 01:28:29 +0530

    Use NOSUPERUSER instead of NOCREATEUSER

--- admin/sql/create_db.sql
+++ admin/sql/create_db.sql
@@ -2,5 +2,5 @@
 
 -- Create the user and the database. Must run as user postgres.
 
-CREATE USER acousticbrainz NOCREATEDB NOCREATEUSER;
+CREATE USER acousticbrainz NOCREATEDB NOSUPERUSER;
 CREATE DATABASE acousticbrainz WITH OWNER = acousticbrainz TEMPLATE template0 ENCODING = 'UNICODE';

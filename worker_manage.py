f54d2c1a4fa11865b7f7f0276b680c105ced0726
Alastair Porter <alastair.porter@upf.edu>
2018-01-31 15:37:46 +0100

    Move worker_manage help text to docstrings

--- worker_manage.py
+++ worker_manage.py
@@ -10,14 +10,16 @@ import webserver
 cli = FlaskGroup(add_default_commands=False, create_app=webserver.create_app)
 
 
-@cli.command('hl_extractor', help='High-level extractor tool')
+@cli.command('hl_extractor')
 @click.option('--threads', '-t', default=1, type=int)
 def command_hl_extractor(threads=1):
+    """Compute high-level features from low-level data files."""
     hl_extractor.hl_calc.main(threads)
 
 
-@cli.command('dataset_evaluator', help='Evaluate pending datasets')
+@cli.command('dataset_evaluator')
 def command_dataset_evaluator():
+    """Evaluate pending datasets."""
     dataset_eval.evaluate.main()
 
 

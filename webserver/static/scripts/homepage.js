04684febfa38567c9294a1da3c859b96460b8310
RashiSah <rashi.747@gmail.com>
2018-02-10 01:25:28 +0530

    getMonth() returns the month (from 0-11) for a particular date

--- webserver/static/scripts/homepage.js
+++ webserver/static/scripts/homepage.js
@@ -4,7 +4,7 @@ $(function() {
     if (lc_time > 0) {
         // lc_time of 0 means never
         var d = new Date(lc_time);
-        var collected_str = d.getFullYear() + "-" + ('0'+d.getMonth()).slice(-2) + "-" +
+        var collected_str = d.getFullYear() + "-" + ('0'+(d.getMonth()+1)).slice(-2) + "-" +
             ('0'+d.getDate()).slice(-2) + " " + ('0'+d.getHours()).slice(-2) + ":" +
             ('0'+d.getMinutes()).slice(-2);
         last_collected.html(collected_str);

fcacaed6454c60817fcddbffe7d4606b0b8000c8
RashiSah <rashi.747@gmail.com>
2018-06-13 19:33:37 +0530

    Add more indexes to musicbrainz indexes file and add an update file

--- admin/sql/create_musicbrainz_indexes.sql
+++ admin/sql/create_musicbrainz_indexes.sql
@@ -4,8 +4,13 @@ CREATE UNIQUE INDEX artist_idx_gid ON musicbrainz.artist (gid);
 CREATE INDEX artist_idx_name ON musicbrainz.artist (name);
 CREATE INDEX artist_idx_sort_name ON musicbrainz.artist (sort_name);
 CREATE INDEX artist_idx_area ON musicbrainz.artist (area);
+CREATE INDEX artist_idx_begin_area ON musicbrainz.artist (begin_area);
+CREATE INDEX artist_idx_end_area ON musicbrainz.artist (end_area);
+
 CREATE UNIQUE INDEX artist_idx_null_comment ON musicbrainz.artist (name) WHERE comment IS NULL;
 CREATE UNIQUE INDEX artist_idx_uniq_name_comment ON musicbrainz.artist (name, comment) WHERE comment IS NOT NULL;
+
+CREATE UNIQUE INDEX area_type_idx_gid ON musicbrainz.area_type (gid);
 
 CREATE UNIQUE INDEX area_idx_gid ON musicbrainz.area (gid);
 CREATE INDEX area_idx_name ON musicbrainz.area (name);
@@ -41,5 +46,22 @@ CREATE INDEX release_group_idx_name ON musicbrainz.release_group (name);
 CREATE INDEX release_group_idx_artist_credit ON musicbrainz.release_group (artist_credit);
 
 CREATE INDEX medium_idx_track_count ON musicbrainz.medium (track_count);
+
+CREATE UNIQUE INDEX medium_format_idx_gid ON musicbrainz.medium_format (gid);
+
+CREATE UNIQUE INDEX release_status_idx_gid ON musicbrainz.release_status (gid);
+
+CREATE UNIQUE INDEX language_idx_iso_code_2b ON musicbrainz.language (iso_code_2b);
+CREATE UNIQUE INDEX language_idx_iso_code_2t ON musicbrainz.language (iso_code_2t);
+CREATE UNIQUE INDEX language_idx_iso_code_1 ON musicbrainz.language (iso_code_1);
+CREATE UNIQUE INDEX language_idx_iso_code_3 ON musicbrainz.language (iso_code_3);
+
+CREATE UNIQUE INDEX release_packaging_idx_gid ON musicbrainz.release_packaging (gid);
+
+CREATE UNIQUE INDEX script_idx_iso_code ON musicbrainz.script (iso_code);
+
+CREATE UNIQUE INDEX gender_idx_gid ON musicbrainz.gender (gid);
+
+CREATE UNIQUE INDEX artist_type_idx_gid ON musicbrainz.artist_type (gid);
 
 COMMIT;

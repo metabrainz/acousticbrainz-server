ca54f8993f0df96e4f8c2dc5bd25a901f1b16a0f
RashiSah <rashi.747@gmail.com>
2018-06-22 15:41:27 +0530

    Import recordings in batches and pick the limit from config file

--- musicbrainz_importer/musicbrainz_importer.py
+++ musicbrainz_importer/musicbrainz_importer.py
@@ -3,6 +3,7 @@ import db.import_mb_data
 import logging
 from sqlalchemy import text
 import time
+from flask import current_app
 
 SLEEP_DURATION = 30  # number of seconds to wait between runs
 
@@ -10,9 +11,10 @@ SLEEP_DURATION = 30  # number of seconds to wait between runs
 def main():
     logging.info("musicbrainz importer started")
     while True:
-        gids_in_AB = get_new_recordings_from_AB()
+        gids_in_AB, rows_to_fetch = get_new_recordings_from_AB()
         if gids_in_AB:
-            logging.info("Updating AcousticBrainz database...")
+            logging.info("Importing MusicBrainz data...")
+            logging.info('Inserting data for %d recordings...' % (rows_to_fetch))
             db.import_mb_data.fetch_and_insert_musicbrainz_data(gids_in_AB)
         else:
             logging.info("No new recording found. Sleeping %s seconds." % SLEEP_DURATION)
@@ -21,13 +23,21 @@ def main():
 
 def get_new_recordings_from_AB():
     with db.engine.begin() as connection:
+        offset = 0
+        rows_to_fetch = current_app.config['RECORDINGS_FETCHED_PER_BATCH']
+
         query = text("""SELECT lowlevel.gid
                           FROM lowlevel
                      LEFT JOIN musicbrainz.recording
                             ON lowlevel.gid = musicbrainz.recording.gid
                          WHERE musicbrainz.recording.gid is NULL
-            """)
-        gids = connection.execute(query)
+                      ORDER BY lowlevel.id
+                        OFFSET :offset
+                         LIMIT :rows_to_fetch
+        """)
+        gids = connection.execute(query, {"offset": offset, "rows_to_fetch": rows_to_fetch})
         gids = gids.fetchall()
         gids_in_AB = [value[0] for value in gids]
-        return gids_in_AB
+        offset = offset + rows_to_fetch
+
+        return gids_in_AB, rows_to_fetch

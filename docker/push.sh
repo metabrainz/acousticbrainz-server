423382d78a7ce0735adb55c9bf2a3ee817d116cb
Param Singh <paramsingh258@gmail.com>
2018-07-14 14:04:55 +0530

    Add push.sh script to push docker images to docker hub

---
+++ docker/push.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# Build images from the currently checked out version of AcousticBrainz
+# and push it to the Docker Hub, with an optional tag (by default "beta").
+#
+# Usage:
+#   $ ./push.sh [env] [tag]
+#
+# Examples:
+#   $ ./push.sh beta beta             # will push image with tag beta and deploy environment beta
+#   $ ./push.sh prod v-2018-07-14.0   # will push images with tag v-2018-07-14.0 and deploy env prod
+
+cd "$(dirname "${BASH_SOURCE[0]}")/../"
+
+git describe --tags --dirty --always > .git-version
+
+ENV=${1:-beta}
+TAG=${2:-beta}
+
+echo "Building AcousticBrainz web image with env $ENV tag $TAG..."
+docker build -t metabrainz/acousticbrainz-web:$TAG \
+        -f Dockerfile.prod \
+        --build-arg GIT_COMMIT_SHA=$(git rev-parse HEAD) \
+        --build-arg deploy_env=$ENV .
+echo "Done!"
+echo "Pushing image to docker hub metabrainz/acousticbrainz-web:$TAG..."
+docker push metabrainz/acousticbrainz-web:$TAG
+echo "Done!"
+
+echo "Building AcousticBrainz gaia image with env $ENV tag $TAG..."
+docker build -t metabrainz/acousticbrainz-gaia:$TAG \
+        -f Dockerfile.gaia.prod \
+        --build-arg GIT_COMMIT_SHA=$(git rev-parse HEAD) \
+        --build-arg deploy_env=$ENV .
+echo "Done!"
+echo "Pushing image to docker hub metabrainz/acousticbrainz-gaia:$TAG..."
+docker push metabrainz/acousticbrainz-gaia:$TAG
+echo "Done!"

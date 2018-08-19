3e00da2fb412bc48869696f15d2a3823e345fb2b
rsh7 <rashi.747@gmail.com>
2018-08-20 02:52:21 +0530

    Revert assert statements for test functions not related to this PR

--- webserver/views/api/v1/test/test_core.py
+++ webserver/views/api/v1/test/test_core.py
@@ -29,12 +29,12 @@ class CoreViewsTestCase(ServerTestCase):
     def test_get_low_level(self):
         mbid = "0dad432b-16cc-4bf0-8961-fd31d124b01b"
         resp = self.client.get("/api/v1/%s/low-level" % mbid)
-        self.assert404(resp)
+        self.assertEqual(resp.status_code, 404)
 
         self.load_low_level_data(mbid)
 
         resp = self.client.get("/api/v1/%s/low-level" % mbid)
-        self.assert200(resp)
+        self.assertEqual(resp.status_code, 200)
 
     def test_submit_low_level(self):
         mbid = "0dad432b-16cc-4bf0-8961-fd31d124b01b"
@@ -44,10 +44,10 @@ class CoreViewsTestCase(ServerTestCase):
                 sub_resp = client.post("/api/v1/%s/low-level" % mbid,
                                        data=json_file.read(),
                                        content_type="application/json")
-                self.assert200(sub_resp)
+                self.assertEqual(sub_resp.status_code, 200)
 
         resp = self.client.get("/api/v1/%s/low-level" % mbid)
-        self.assert200(resp)
+        self.assertEqual(resp.status_code, 200)
 
     def test_cors_headers(self):
         mbid = "0dad432b-16cc-4bf0-8961-fd31d124b01b"
@@ -65,11 +65,11 @@ class CoreViewsTestCase(ServerTestCase):
             This error is raised by Flask, but we special-case to json.
         """
         resp = self.client.get("/api/v1/nothing/low-level")
-        self.assert404(resp)
+        self.assertEqual(404, resp.status_code)
         load_low_level.assert_not_called()
         
         expected_result = {"message": "The requested URL was not found on the server.  If you entered the URL manually please check your spelling and try again."}
-        self.assertDictEqual(resp.json, expected_result)
+        self.assertEqual(resp.json, expected_result)
 
     @mock.patch("db.data.load_low_level")
     def test_ll_internal_server_error(self, load_low_level):
@@ -83,7 +83,7 @@ class CoreViewsTestCase(ServerTestCase):
 
         load_low_level.side_effect = ValueError
         resp = self.client.get("/api/v1/%s/low-level" % self.uuid)
-        self.assert500(resp)
+        self.assertEqual(500, resp.status_code)
 
         expected_result = {"message": "An unknown error occurred"}
         self.assertDictEqual(resp.json, expected_result)
@@ -93,43 +93,43 @@ class CoreViewsTestCase(ServerTestCase):
     def test_ll_no_offset(self, ll):
         ll.return_value = {}
         resp = self.client.get("/api/v1/%s/low-level" % self.uuid)
-        self.assert200(resp)
+        self.assertEqual(200, resp.status_code)
         ll.assert_called_with(self.uuid, 0)
 
     @mock.patch("db.data.load_low_level")
     def test_ll_numerical_offset(self, ll):
         ll.return_value = {}
         resp = self.client.get("/api/v1/%s/low-level?n=3" % self.uuid)
-        self.assert200(resp)
+        self.assertEqual(200, resp.status_code)
         ll.assert_called_with(self.uuid, 3)
 
     @mock.patch("db.data.load_low_level")
     def test_ll_bad_offset(self, ll):
         resp = self.client.get("/api/v1/%s/low-level?n=x" % self.uuid)
-        self.assert400(resp)
+        self.assertEqual(400, resp.status_code)
 
     @mock.patch("db.data.load_low_level")
     def test_ll_no_item(self, ll):
         ll.side_effect = db.exceptions.NoDataFoundException
         resp = self.client.get("/api/v1/%s/low-level" % self.uuid)
-        self.assert404(resp)
+        self.assertEqual(404, resp.status_code)
         self.assertEqual("Not found", resp.json["message"])
 
     @mock.patch("db.data.load_high_level")
     def test_hl_numerical_offset(self, hl):
         hl.return_value = {}
         resp = self.client.get("/api/v1/%s/high-level?n=3" % self.uuid)
-        self.assert200(resp)
+        self.assertEqual(200, resp.status_code)
         hl.assert_called_with(self.uuid, 3)
 
     @mock.patch('db.data.load_low_level')
     def test_get_bulk_ll_no_param(self, load_low_level):
         # No parameter in bulk lookup results in an error
         resp = self.client.get('api/v1/low-level')
-        self.assert400(resp)
+        self.assertEqual(resp.status_code, 400)
 
         expected_result = {"message": "Missing `recording_ids` parameter"}
-        self.assertDictEqual(resp.json, expected_result)
+        self.assertEqual(resp.json, expected_result)
 
     @mock.patch('db.data.load_low_level')
     def test_get_bulk_ll(self, load_low_level):
@@ -146,14 +146,14 @@ class CoreViewsTestCase(ServerTestCase):
         load_low_level.side_effect = [rec_c5, rec_7f, rec_40_2, rec_40_3]
 
         resp = self.client.get('api/v1/low-level?recording_ids=' + params)
-        self.assert200(resp)
+        self.assertEqual(resp.status_code, 200)
 
         expected_result = {
             "c5f4909e-1d7b-4f15-a6f6-1af376bc01c9": {"0": rec_c5},
             "7f27d7a9-27f0-4663-9d20-2c9c40200e6d": {"3": rec_7f},
             "405a5ff4-7ee2-436b-95c1-90ce8a83b359": {"2": rec_40_2, "3": rec_40_3}
         }
-        self.assertDictEqual(resp.json, expected_result)
+        self.assertEqual(resp.json, expected_result)
 
         calls = [mock.call("c5f4909e-1d7b-4f15-a6f6-1af376bc01c9", 0),
                  mock.call("7f27d7a9-27f0-4663-9d20-2c9c40200e6d", 3),
@@ -174,13 +174,13 @@ class CoreViewsTestCase(ServerTestCase):
         load_low_level.side_effect = [rec_c5, db.exceptions.NoDataFoundException, rec_40_2]
 
         resp = self.client.get('api/v1/low-level?recording_ids=' + params)
-        self.assert200(resp)
+        self.assertEqual(resp.status_code, 200)
 
         expected_result = {
             "c5f4909e-1d7b-4f15-a6f6-1af376bc01c9": {"0": rec_c5},
             "405a5ff4-7ee2-436b-95c1-90ce8a83b359": {"2": rec_40_2},
         }
-        self.assertDictEqual(resp.json, expected_result)
+        self.assertEqual(resp.json, expected_result)
 
         calls = [mock.call("c5f4909e-1d7b-4f15-a6f6-1af376bc01c9", 0),
                  mock.call("7f27d7a9-27f0-4663-9d20-2c9c40200e6d", 3),
@@ -192,7 +192,7 @@ class CoreViewsTestCase(ServerTestCase):
         manyids = [str(uuid.uuid4()) for i in range(205)]
         limit_exceed_url = ";".join(manyids)
         resp = self.client.get('api/v1/low-level?recording_ids=' + limit_exceed_url)
-        self.assert400(resp)
+        self.assertEqual(resp.status_code, 400)
         self.assertEqual('More than 200 recordings not allowed per request', resp.json['message'])
 
     @mock.patch('db.data.load_high_level')
@@ -286,7 +286,7 @@ class CoreViewsTestCase(ServerTestCase):
 
         for mbid in mbids:
             resp = self.client.get('api/v1/%s/count' % mbid)
-            self.assert200(resp)
+            self.assertEqual(resp.status_code, 200)
             self.assertDictEqual(resp.json,
                                  {"mbid": mbid,
                                   "count": expected_result[mbid]})
@@ -294,12 +294,12 @@ class CoreViewsTestCase(ServerTestCase):
     def test_get_bulk_count_no_param(self):
         # No parameter in bulk lookup results in an error
         resp = self.client.get('api/v1/count')
-        self.assert400(resp)
+        self.assertEqual(resp.status_code, 400)
 
     def test_get_bulk_count(self):
         mbids = self.submit_fake_data()
         resp = self.client.get('api/v1/count?recording_ids=' + ';'.join(mbids))
-        self.assert200(resp)
+        self.assertEqual(resp.status_code, 200)
 
         expected_result = {
             "7f27d7a9-27f0-4663-9d20-2c9c40200e6d": {"count": 1},
@@ -312,7 +312,7 @@ class CoreViewsTestCase(ServerTestCase):
         manyids = [str(uuid.uuid4()) for i in range(205)]
         limit_exceed_url = ";".join(manyids)
         resp = self.client.get('api/v1/count?recording_ids=' + limit_exceed_url)
-        self.assert400(resp)
+        self.assertEqual(resp.status_code, 400)
         self.assertEqual('More than 200 recordings not allowed per request',
                          resp.json['message'])
 

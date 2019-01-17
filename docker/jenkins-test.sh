4f3e4331aa8973053bb28af2ef92b411fd5b6b29
Param Singh <param@metabrainz.org>
2019-01-17 19:17:06 +0530

    Remove --name from db creation command

--- docker/jenkins-test.sh
+++ docker/jenkins-test.sh
@@ -46,7 +46,7 @@ function run_tests {
 
     # create database and user once
     echo "creating user and database"
-    docker-compose -f $COMPOSE_FILE_LOC -p $COMPOSE_PROJECT_NAME run --rm --name $TEST_CONTAINER_REF $TEST_CONTAINER_NAME \
+    docker-compose -f $COMPOSE_FILE_LOC -p $COMPOSE_PROJECT_NAME run --rm $TEST_CONTAINER_NAME \
         dockerize \
         -wait tcp://db:5432 -timeout 60s bash -c \
         "cd /code && cp config.py.example config.py && ls -lR && python manage.py init_db"

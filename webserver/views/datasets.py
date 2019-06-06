metabrainz/acousticbrainz-server

commit 06e8968feea3cbafe65f48c2f5734d561519ae6a

Make a new generic test to check views that use get_dataset

webserver/views/datasets.py
@@ -83,9 +83,9 @@ def list_datasets(status):
             nextpage=nextpage)
 
 
-@datasets_bp.route("/<uuid:id>")
-def view(id):
-    ds = get_dataset(id)
+@datasets_bp.route("/<uuid:dataset_id>")
+def view(dataset_id):
+    ds = get_dataset(dataset_id)
     return render_template(
         "datasets/view.html",
         dataset=ds,
@@ -212,7 +212,7 @@ def evaluate(dataset_id):
         db.dataset_eval.validate_dataset_structure(ds)
     except db.dataset_eval.IncompleteDatasetException as e:
         flash.error("Cannot add this dataset because of a validation error: %s" % e)
-        return redirect(url_for("datasets.view", id=dataset_id))
+        return redirect(url_for("datasets.view", dataset_id=dataset_id))
 
     form = forms.DatasetEvaluationForm()
 

webserver/templates/datasets/list.html
@@ -40,7 +40,7 @@
           </a>
         </td>
         <td>
-          <a href="{{ url_for('datasets.view', id=ds['id']) }}">{{ ds['name'] }}</a>
+          <a href="{{ url_for('datasets.view', dataset_id=ds['id']) }}">{{ ds['name'] }}</a>
         </td>
         <td>{{ ds['created']|datetime }}</td>
         <td>{{ render_job_status(ds['status']) }}</td>

webserver/templates/datasets/macros.html
@@ -49,7 +49,7 @@
 
     <ul class="nav nav-tabs">
       <li {{ 'class=active' if active_tab=='view' }}>
-        <a href="{{ url_for('datasets.view', id=dataset['id']) }}">View</a>
+        <a href="{{ url_for('datasets.view', dataset_id=dataset['id']) }}">View</a>
       </li>
       <li {{ 'class=active' if active_tab=='evaluation' }}>
         <a href="{{ url_for('datasets.eval_info', dataset_id=dataset['id']) }}">Evaluation</a>

webserver/templates/user/profile.html
@@ -50,7 +50,7 @@
       <tbody>
       {% for dataset in datasets %}
         <tr>
-          <td><a href="{{ url_for('datasets.view', id=dataset['id']) }}">{{ dataset['name'] }}</a></td>
+          <td><a href="{{ url_for('datasets.view', dataset_id=dataset['id']) }}">{{ dataset['name'] }}</a></td>
           <td>{{ dataset['created']|datetime }}</td>
           <td>
             {% if own_page %}

webserver/views/test/test_datasets.py
@@ -54,24 +54,10 @@ class DatasetsViewsTestCase(ServerTestCase):
         self.load_low_level_data(self.test_mbid_2)
 
     def test_view(self):
-        resp = self.client.get(url_for("datasets.view", id=self.test_uuid))
-        self.assert404(resp)
-
-        dataset_id = dataset.create_from_dict(self.test_data, author_id=self.test_user_id)
-        resp = self.client.get(url_for("datasets.view", id=dataset_id))
-        self.assert200(resp)
+        self._test_view_with_get_dataset("datasets.view")
 
     def test_view_json(self):
-        resp = self.client.get(url_for("datasets.view_json", id=self.test_uuid))
-        self.assert404(resp)
-
-        dataset_id = dataset.create_from_dict(self.test_data, author_id=self.test_user_id)
-        resp = self.client.get(url_for("datasets.view_json", id=dataset_id))
-        self.assert200(resp)
-
-        dataset_eval.evaluate_dataset(dataset_id, False, dataset_eval.EVAL_LOCAL)
-
-        self.temporary_login(self.test_user_id)
+        self._test_view_with_get_dataset("datasets.view_json")
 
     def test_eval_job_delete(self):
         resp = self.client.delete(url_for("datasets.eval_job", dataset_id=self.test_uuid, job_id=self.test_uuid))
@@ -151,7 +137,6 @@ class DatasetsViewsTestCase(ServerTestCase):
         self.assert200(resp)
 
     def test_edit_service(self):
-
         dataset_id = dataset.create_from_dict(self.test_data, author_id=self.test_user_id)
 
         # Trying to edit without login
@@ -215,6 +200,36 @@ class DatasetsViewsTestCase(ServerTestCase):
         resp = self.client.post(url_for("datasets.delete", dataset_id=dataset_id))
         self.assertRedirects(resp, url_for("user.profile", musicbrainz_id=self.test_user_mb_name))
         self.assertTrue(len(dataset.get_by_user_id(self.test_user_id)) == 0)
+
+    def _test_view_with_get_dataset(self, view_name):
+        """Check that a view that uses datasets.get_dataset to retrieve a dataset"""
+        # no such dataset, 404
+        resp = self.client.get(url_for(view_name, dataset_id=self.test_uuid))
+        self.assert404(resp)
+
+        # public dataset + not logged in, OK
+        dataset_id = dataset.create_from_dict(self.test_data, author_id=self.test_user_id)
+        resp = self.client.get(url_for(view_name, dataset_id=dataset_id))
+        self.assert200(resp)
+
+        self.temporary_login(self.test_user_id)
+
+        # private dataset + logged in, ok
+        resp = self.client.get(url_for(view_name, dataset_id=dataset_id))
+        self.assert200(resp)
+
+        # private dataset + author, ok
+        self.test_data["public"] = False
+        private_dataset_id = dataset.create_from_dict(self.test_data, author_id=self.test_user_id)
+        resp = self.client.get(url_for(view_name, dataset_id=private_dataset_id))
+        self.assert200(resp)
+
+        # private dataset, not author, 404
+        another_user_id = user.create("another_tester")
+        user.agree_to_gdpr("another_tester")
+        self.temporary_login(another_user_id)
+        resp = self.client.get(url_for(view_name, dataset_id=private_dataset_id))
+        self.assert404(resp)
 
     @mock.patch('webserver.external.musicbrainz.get_recording_by_id')
     def test_recording_info(self, get_recording_by_id):

commit 887e4c03fca4ad9bc9f8a90e8309ec20394eeebe

Fix redirect to dataset view broken in 06e8968f
Parameter name to view was changed but call to url_for wasn't updated

webserver/views/datasets.py
@@ -310,7 +310,7 @@ def import_csv():
         except dataset_validator.ValidationException as e:
             raise BadRequest(str(e))
         flash.info("Dataset has been imported successfully.")
-        return redirect(url_for(".view", id=dataset_id))
+        return redirect(url_for(".view", dataset_id=dataset_id))
 
     else:
         return render_template("datasets/import.html", form=form)
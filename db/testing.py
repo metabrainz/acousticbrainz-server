9db618e63f1c89461a4c684210b24238678ba7c1
Aidan Lawford-Wickham <aidan.lw17@gmail.com>
2019-06-30 01:22:55 -0400

    Refactor sql for dropping tables and types

--- db/testing.py
+++ db/testing.py
@@ -41,39 +41,10 @@ class DatabaseTestCase(TestCase):
         db.run_sql_script(os.path.join(ADMIN_SQL_DIR, 'create_indexes.sql'))
 
     def drop_tables(self):
-        with db.engine.connect() as connection:
-            # TODO(roman): See if there's a better way to drop all tables.
-            connection.execute('DROP TABLE IF EXISTS highlevel_model        CASCADE;')
-            connection.execute('DROP TABLE IF EXISTS highlevel_meta         CASCADE;')
-            connection.execute('DROP TABLE IF EXISTS highlevel              CASCADE;')
-            connection.execute('DROP TABLE IF EXISTS model                  CASCADE;')
-            connection.execute('DROP TABLE IF EXISTS lowlevel_json          CASCADE;')
-            connection.execute('DROP TABLE IF EXISTS lowlevel               CASCADE;')
-            connection.execute('DROP TABLE IF EXISTS version                CASCADE;')
-            connection.execute('DROP TABLE IF EXISTS statistics             CASCADE;')
-            connection.execute('DROP TABLE IF EXISTS incremental_dumps      CASCADE;')
-            connection.execute('DROP TABLE IF EXISTS dataset_snapshot       CASCADE;')
-            connection.execute('DROP TABLE IF EXISTS dataset_eval_jobs      CASCADE;')
-            connection.execute('DROP TABLE IF EXISTS dataset_class_member   CASCADE;')
-            connection.execute('DROP TABLE IF EXISTS dataset_class          CASCADE;')
-            connection.execute('DROP TABLE IF EXISTS dataset                CASCADE;')
-            connection.execute('DROP TABLE IF EXISTS dataset_eval_sets      CASCADE;')
-            connection.execute('DROP TABLE IF EXISTS "user"                 CASCADE;')
-            connection.execute('DROP TABLE IF EXISTS api_key                CASCADE;')
-            connection.execute('DROP TABLE IF EXISTS challenge              CASCADE;')
-            connection.execute('DROP TABLE IF EXISTS dataset_eval_challenge CASCADE;')
-            connection.execute('DROP TABLE IF EXISTS feedback               CASCADE;')
-            connection.execute('DROP TABLE IF EXISTS similarity             CASCADE;')
-            connection.execute('DROP TABLE IF EXISTS similarity_metrics     CASCADE;')
-            connection.execute('DROP TABLE IF EXISTS similarity_stats       CASCADE;')
+        db.run_sql_script(os.path.join(ADMIN_SQL_DIR, 'drop_tables.sql'))
 
     def drop_types(self):
-        with db.engine.connect() as connection:
-            connection.execute('DROP TYPE IF EXISTS eval_job_status CASCADE;')
-            connection.execute('DROP TYPE IF EXISTS model_status CASCADE;')
-            connection.execute('DROP TYPE IF EXISTS version_type CASCADE;')
-            connection.execute('DROP TYPE IF EXISTS eval_location_type CASCADE;')
-            connection.execute('DROP TYPE IF EXISTS gid_type CASCADE;')
+        db.run_sql_script(os.path.join(ADMIN_SQL_DIR, 'drop_types.sql'))
 
     def data_filename(self, mbid):
         """ Get the expected filename of a test datafile given its mbid """

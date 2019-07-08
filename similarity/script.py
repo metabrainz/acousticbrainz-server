dae1330ae66c409828acfb02c0c7e2ee3e8c0411
Aidan Lawford-Wickham <aidan.lw17@gmail.com>
2019-07-08 18:34:01 +0100

    Remove unneeded imports in similarity/script.py

--- similarity/script.py
+++ similarity/script.py
@@ -1,23 +1,18 @@
 from __future__ import print_function
 from flask.cli import FlaskGroup
-import timeit
-import time
 import click
 
 import webserver
 import db
 
-from utils import get_all_metrics, get_all_indices
 from index_model import AnnoyModel
 
-from sqlalchemy import text
-from collections import defaultdict
 
 PROCESS_BATCH_SIZE = 10000
 
 cli = FlaskGroup(add_default_commands=False, create_app=webserver.create_app_flaskgroup)
 
-mbids=[
+mbids = [
     'ebf79ba5-085e-48d2-9eb8-2d992fbf0f6d',
     '8d5f76cf-0fa1-45a1-8464-68053d03b46b',
     'c718f7c1-b63b-4638-bda3-42ca56177dd7',
@@ -31,29 +26,12 @@ mbids=[
 def probe_annoy():
     """Get similar recordings using the annoy index."""
     with db.engine.connect() as connection:
-        index = AnnoyModel(connection, "mfccs", load_existing=True)
+        index = AnnoyModel("mfccs", load_existing=True)
         recordings = index.get_nns_by_mbid(mbids[0], 0, 1000)
 
-        # print("Similar recordings:")
-        # print(recordings)
-        # print("========================")
-        # query = text("""
-        #     SELECT gid
-        #     FROM lowlevel
-        #     WHERE id
-        #     IN :recordings
-        # """)
-        # result = connection.execute(query, { "recordings": tuple(recordings) })
-
-        # recordings = []
-        # for row in result.fetchall():
-        #     recordings.append(row["gid"])
-        # print("Similar recordings:")
-        # print(recordings)
-        # print("===================")
         ret = []
         for mbid, offset in recordings:
             ret.append(mbid)
 
-        print(ret)
+        click.echo(ret)
         return ret

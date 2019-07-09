dfb2d7d27edc2d66c8fecb21a9d2e2d894a5d813
Aidan Lawford-Wickham <aidan.lw17@gmail.com>
2019-07-10 00:31:24 +0100

    Refactoring, docstrings, and adding function to initialize metrics

--- similarity/utils.py
+++ similarity/utils.py
@@ -2,6 +2,8 @@ import similarity.metrics
 
 
 def init_metrics(force=False):
+    """Initializes and creates all base metrics, returning
+    their instances as a list."""
     metrics = []
     for name in similarity.metrics.BASE_METRICS:
         metric_cls = similarity.metrics.BASE_METRICS[name]

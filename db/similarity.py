c0c3a0d65692587e15dac21a1af03ac010a7d760
Aidan Lawford-Wickham <aidan.lw17@gmail.com>
2019-07-10 22:57:26 +0100

    Remove functions to add/delete metrics and metric metadata

--- db/similarity.py
+++ db/similarity.py
@@ -4,18 +4,19 @@ import db
 from db.data import count_all_lowlevel
 from db.exceptions import NoDataFoundException, BadDataException
 import similarity.metrics
+import similarity.utils
 
 from sqlalchemy import text
 
 PROCESS_BATCH_SIZE = 10000
 
 
-def add_metrics(force=False, batch_size=None):
+def add_metrics(batch_size=None):
     batch_size = batch_size or PROCESS_BATCH_SIZE
     lowlevel_count = count_all_lowlevel()
 
     with db.engine.connect() as connection:
-        metrics = similarity.utils.init_metrics(force=force)
+        metrics = similarity.utils.init_metrics()
         offset = count_similarity()
         print("Processed {} / {} ({:.3f}%)".format(offset,
                                                    lowlevel_count,
@@ -130,64 +131,3 @@ def submit_similarity_by_mbid(mbid, offset):
     row in the similarity table."""
     id = db.data.get_lowlevel_id(mbid, offset)
     submit_similarity_by_id(id)
-
-
-# TODO: Write tests for these and for stats, also add docstrings.
-def insert_similarity_meta(metric, hybrid, description, category):
-    with db.engine.connect() as connection:
-        metrics_query = text("""
-            INSERT INTO similarity_metrics (metric, is_hybrid, description, category, visible)
-                 VALUES (:metric, :hybrid, :description, :category, TRUE)
-            ON CONFLICT (metric)
-          DO UPDATE SET visible=TRUE
-        """)
-        connection.execute(metrics_query, {'metric': metric,
-                                           'hybrid': hybrid,
-                                           'description': description,
-                                           'category': category})
-
-
-def delete_similarity_meta(metric):
-    with db.engine.connect() as connection:
-        query = text("""
-            DELETE FROM similarity_metrics
-                  WHERE metric = :metric
-        """)
-        connection.execute(query, {"metric": metric})
-
-
-def create_similarity_metric(metric, clear):
-    with db.engine.connect() as connection:
-        query = text("""
-            ALTER TABLE similarity
-             ADD COLUMN
-          IF NOT EXISTS %s DOUBLE PRECISION[]
-        """ % metric)
-        connection.execute(query)
-
-        if clear:
-            # Delete all existing rows.
-            query = text("""
-                DELETE FROM ONLY similarity
-            """)
-        connection.execute(query)
-
-
-def delete_similarity_metric(metric):
-    with db.engine.connect() as connection:
-        query = text("""
-            ALTER TABLE similarity
-            DROP COLUMN
-              IF EXISTS %s
-        """ % metric)
-        connection.execute(query)
-
-
-def remove_visibility(metric):
-    with db.engine.connect() as connection:
-        query = text("""
-            UPDATE similarity_metrics
-               SET visible = FALSE
-             WHERE metric = :metric
-        """)
-        connection.execute(query, {"metric": metric})

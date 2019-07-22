bac88a11977f5c2ed0eb8089a4eb4bd8d40bb34b
Aidan Lawford-Wickham <aidan.lw17@gmail.com>
2019-07-22 09:18:02 +0100

    Improve documentation and fix default batch size

--- similarity/manage.py
+++ similarity/manage.py
@@ -10,11 +10,20 @@ cli = FlaskGroup(add_default_commands=False, create_app=webserver.create_app_fla
 
 
 @cli.command(name="add-metrics")
-@click.option("--batch-size", "-b", type=int, help="Override processing batch size.")
-def add_metrics(batch_size=10000):
+@click.option("--batch-size", "-b", type=int, default=10000, help="Override processing batch size.")
+def add_metrics(batch_size):
     """Computes all 12 base metrics for each recording
     in the lowlevel table, inserting these values in
-    the similarity table."""
+    the similarity table. 
+    
+    Requires fetching lowlevel data and the highlevel 
+    models for each recording.
+    
+    Args: 
+        batch_size: integer, number of recordings that 
+        should be added on each iteration. 
+        Suggested value between 10 000 and 20 000.
+    """
     click.echo("Adding all metrics...")
-    db.similarity.add_metrics(batch_size=batch_size)
+    db.similarity.add_metrics(batch_size)
     click.echo("Finished adding all metrics, exiting...")

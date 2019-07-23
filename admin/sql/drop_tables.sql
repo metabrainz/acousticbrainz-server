02d663ea8a9ccb1788102d6ed8e08cbcb3fd6207
Aidan Lawford-Wickham <aidan.lw17@gmail.com>
2019-07-23 09:17:37 +0100

    Adds similarity schema

--- admin/sql/drop_tables.sql
+++ admin/sql/drop_tables.sql
@@ -20,9 +20,5 @@ DROP TABLE IF EXISTS api_key                CASCADE;
 DROP TABLE IF EXISTS challenge              CASCADE;
 DROP TABLE IF EXISTS dataset_eval_challenge CASCADE;
 DROP TABLE IF EXISTS feedback               CASCADE;
-DROP TABLE IF EXISTS similarity             CASCADE;
-DROP TABLE IF EXISTS similarity_metrics     CASCADE;
-DROP TABLE IF EXISTS similarity_stats       CASCADE;
-DROP TABLE IF EXISTS similarity_eval        CASCADE;
 
 COMMIT;

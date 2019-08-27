49b35799138d7dd01a6f095b1045c411be18a7cc
Aidan Lawford-Wickham <aidan.lw17@gmail.com>
2019-08-27 12:24:00 -0400

    Add add-indices to init command and reorganize command options

--- similarity/manage.py
+++ similarity/manage.py
@@ -14,24 +14,40 @@ NORMALIZATION_SAMPLE_SIZE = 10000
 cli = FlaskGroup(add_default_commands=False, create_app=webserver.create_app_flaskgroup)
 
 
-@cli.command(name="add-metrics")
-@click.option("--batch-size", "-b", type=int, default=None, help="Override processing batch size.")
-def add_metrics(batch_size):
-    """Computes all 12 base metrics for each recording
-    in the lowlevel table, inserting these values in
-    the similarity.similarity table.
+@cli.command(name="init")
+@click.option("--distance-type", "-d", default='angular')
+@click.option("--n-trees", "-n", type=int, default=10, help="Number of trees for building index. \
+                                                            Tradeoff: more trees gives more precision, \
+                                                            but takes longer to build.")
+@click.option("--force", "-f", default=False, help="Remove existing stats before computing.")
+@click.option("--sample-size", "-s", type=int, default=NORMALIZATION_SAMPLE_SIZE, \
+    help="Override normalization lowlevel data sample size. \
+         Must be >= 1% of lowlevel_json entries.")
+@click.option("--batch-size", "-b", type=int, default=10000, help="Override processing batch size.")
+def init(batch_size, sample_size, force, n_trees, distance_type):
+    """Initialization command for the similarity engine.
+    The following steps will occur:
+        1. Compute global stats required for similarity
+           using a sample of the lowlevel_json table.
+        2. Compute base metrics for all recordings in
+           the lowlevel table, inserting these values
+           in the similarity.similarity table.
 
-    Requires fetching lowlevel data and the highlevel
-    models for each recording.
-
-    Args:
-        batch_size: integer, number of recordings that
-        should be added on each iteration.
-        Suggested value between 10 000 and 20 000.
+    Options:
+        --sample-size: alters sample size when computing stats.
+        --batch-size: alters batch size when adding metrics 
+        incrementally.
     """
-    click.echo("Adding all metrics...")
+    click.echo("Computing stats...")
+    db.similarity_stats.compute_stats(sample_size, force=force)
+    click.echo("Finished computing stats. Adding all metrics...")
     db.similarity.add_metrics(batch_size)
-    click.echo("Finished adding all metrics, exiting...")
+    click.echo("Finished adding metrics. Adding all indices...")
+    click.echo("Initializing indices...")
+    indices = similarity.index_utils.initialize_indices(n_trees=n_trees, distance_type=distance_type)
+    click.echo("Inserting items...")
+    db.similarity.add_indices(indices, batch_size=batch_size)
+    click.echo("Finished adding indices, exiting...")
 
 
 @cli.command(name="compute-stats")
@@ -61,38 +77,33 @@ def compute_stats(sample_size, force):
     click.echo("Finished!")
 
 
-@cli.command(name="init")
-@click.option("--force", "-f", default=False, help="Remove existing stats before computing.")
-@click.option("--sample-size", "-s", type=int, default=NORMALIZATION_SAMPLE_SIZE, \
-    help="Override normalization lowlevel data sample size. \
-         Must be >= 1% of lowlevel_json entries.")
-@click.option("--batch-size", "-b", type=int, default=10000, help="Override processing batch size.")
-def init(batch_size, sample_size, force):
-    """Initialization command for the similarity engine.
-    The following steps will occur:
-        1. Compute global stats required for similarity
-           using a sample of the lowlevel_json table.
-        2. Compute base metrics for all recordings in
-           the lowlevel table, inserting these values
-           in the similarity.similarity table.
+@cli.command(name="add-metrics")
+@click.option("--batch-size", "-b", type=int, default=None, help="Override processing batch size.")
+def add_metrics(batch_size):
+    """Computes all 12 base metrics for each recording
+    in the lowlevel table, inserting these values in
+    the similarity.similarity table.
 
-    Options:
-        --sample-size: alters sample size when computing stats.
-        --batch-size: alters batch size when adding metrics 
-        incrementally.
+    Requires fetching lowlevel data and the highlevel
+    models for each recording.
+
+    Args:
+        batch_size: integer, number of recordings that
+        should be added on each iteration.
+        Suggested value between 10 000 and 20 000.
     """
-    click.echo("Computing stats...")
-    db.similarity_stats.compute_stats(sample_size, force=force)
-    click.echo("Finished computing stats. Adding all metrics...")
+    click.echo("Adding all metrics...")
     db.similarity.add_metrics(batch_size)
     click.echo("Finished adding all metrics, exiting...")
+
+
 @cli.command(name='add-index')
 @click.argument("metric")
-@click.option("--batch_size", "-b", type=int, default=None, help="Size of batches")
+@click.option("--distance_type", "-d", default='angular', help="Method of measuring distance between metric vectors")
 @click.option("--n_trees", "-n", type=int, default=10, help="Number of trees for building index. \
                                                             Tradeoff: more trees gives more precision, \
                                                             but takes longer to build.")
-@click.option("--distance_type", "-d", default='angular', help="Method of measuring distance between metric vectors")
+@click.option("--batch_size", "-b", type=int, default=10000, help="Size of batches")
 def add_index(metric, batch_size, n_trees, distance_type):
     """Creates an annoy index for the specified metric using the given params.
     This operates by creating a special case of `db.similarity.add_indices`,
@@ -110,11 +121,11 @@ def add_index(metric, batch_size, n_trees, distance_type):
 
 
 @cli.command(name='add-indices')
-@click.option("--batch_size", "-b", type=int, default=None, help="Size of batches")
+@click.option("--distance-type", "-d", default='angular')
 @click.option("--n-trees", "-n", type=int, default=10, help="Number of trees for building index. \
                                                             Tradeoff: more trees gives more precision, \
                                                             but takes longer to build.")
-@click.option("--distance-type", "-d", default='angular')
+@click.option("--batch_size", "-b", type=int, default=None, help="Size of batches")
 def add_indices(batch_size, n_trees, distance_type):
     """Creates an annoy index then adds all recordings to the index,
     for each of the base metrics.
@@ -131,10 +142,10 @@ def add_indices(batch_size, n_trees, distance_type):
 
 @cli.command(name='remove-index')
 @click.argument("metric")
+@click.option("--distance_type", "-d", default='angular', help="Method of measuring distance between metric vectors.")
 @click.option("--n_trees", "-n", type=int, default=10, help="Number of trees for building index. \
                                                             Tradeoff: more trees gives more precision, \
                                                             but takes longer to build.")
-@click.option("--distance_type", "-d", default='angular', help="Method of measuring distance between metric vectors.")
 def remove_index(metric, n_trees, distance_type):
     """Removes the index with the specified parameters, if it exists.
 
@@ -147,10 +158,10 @@ def remove_index(metric, n_trees, distance_type):
 
 
 @cli.command(name='remove-indices')
+@click.option("--distance_type", "-d", default='angular', help="Method of measuring distance between metric vectors.")
 @click.option("--n_trees", "-n", type=int, default=10, help="Number of trees for building index. \
                                                             Tradeoff: more trees gives more precision, \
                                                             but takes longer to build.")
-@click.option("--distance_type", "-d", default='angular', help="Method of measuring distance between metric vectors.")
 def remove_indices(n_trees, distance_type):
     """Removes indices for each of the following metrics, if they
     exist with the specified parameters."""

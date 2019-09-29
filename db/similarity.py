39ae5d98da415bb1b74a16aebc1a04651381f90d
Aidan Lawford-Wickham <aidan.lw17@gmail.com>
2019-09-29 17:11:56 +0200

    Split submit_similarity_by_id into bulk and non-bulk methods

--- db/similarity.py
+++ db/similarity.py
@@ -278,41 +278,24 @@ def count_similarity():
         return result.fetchone()[0]
 
 
-def submit_similarity_by_id(id, data=None, metrics=None, connection=None):
+def submit_similarity_by_id(id):
     """Computes similarity metrics for a single recording specified
     by lowlevel.id, then inserts the metrics as a new row in the
-    similarity table.
+    similarity table. Data will be collected before submission and
+    metrics will be initialized.
     
     Args:
-        id: lowlevel.id for desired submission.
-        
-        data: a list (lowlevel_data, highlevel_models). Defaults to None,
-        in which case the data will be collected before submission.
-        
-        metrics: a list of initialized metric classes, for which similarity
-        vectors should be computed and submitted. Default is None, in which
-        case base metrics will be initialized.
-
-        connection: a connection to the database can be specified if this
-        submission should be part of an ongoing transaction.
+        id (int): lowlevel.id for desired submission.
     """
-    try:
-        id = int(id)
-    except ValueError:
-        raise BadDataException('Parameter `id` must be an integer.')
+    metrics = similarity.utils.init_metrics()
+    # Collect and assign stats to metrics that require normalization
+    for metric in metrics:
+        db.similarity_stats.assign_stats(metric)
 
-    if not metrics:
-        metrics = similarity.utils.init_metrics()
-        # Collect and assign stats to metrics that require normalization
-        for metric in metrics:
-            db.similarity_stats.assign_stats(metric)
-
-    if not data:
-        # When a single recording is submitted, not in batch submission,
-        # data can be computed here.
-        ll_data = db.data.get_lowlevel_by_id(id)
-        models = db.data.get_highlevel_models(id)
-        data = (ll_data, models)
+    # Collect high and lowlevel data
+    ll_data = db.data.get_lowlevel_by_id(id)
+    models = db.data.get_highlevel_models(id)
+    data = (ll_data, models)
 
     vectors = []
     metric_names = []
@@ -330,17 +313,60 @@ def submit_similarity_by_id(id, data=None, metrics=None, connection=None):
         vectors.append(vector)
         metric_names.append(metric.name)
 
-    if connection:
+    with db.engine.connect() as connection:
         insert_similarity(connection, id, vectors, metric_names)
-    else:
-        with db.engine.connect() as connection:
-            insert_similarity(connection, id, vectors, metric_names)
+
+
+def bulk_submit_similarity_by_id(id, data, metrics, connection):
+    """Computes similarity metrics for a single recording specified
+    by lowlevel.id, then inserts the metrics as a new row in the
+    similarity table. The bulk case of similarity submission is meant
+    to be used in combination with add_metrics, in which data and metrics
+    are already collected and initialized in bulk.
+    
+    Args:
+        id (int): lowlevel.id for desired submission.
+        
+        data (list): Of the form (lowlevel_data, highlevel_models). Defaults
+        to None, in which case the data will be collected before submission.
+        
+        metrics (list): a list of initialized metric classes, for which similarity
+        vectors should be computed and submitted. Default is None, in which
+        case base metrics will be initialized.
+
+        connection: a connection to the database can be specified if this
+        submission should be part of an ongoing transaction.
+    """
+    vectors = []
+    metric_names = []
+    for metric in metrics:
+        try:
+            metric_data = metric.get_feature_data(data[0])
+        except AttributeError:
+            # High level metrics use models for transformation.
+            metric_data = data[1]
+
+        try:
+            vector = metric.transform(metric_data)
+        except ValueError:
+            vector = [0] * metric.length()
+        vectors.append(vector)
+        metric_names.append(metric.name)
+
+        insert_similarity(connection, id, vectors, metric_names)
 
 
 def submit_similarity_by_mbid(mbid, offset):
     """Computes similarity metrics for a single recording specified
     by (mbid, offset) combination, then inserts the metrics as a new
-    row in the similarity table."""
+    row in the similarity table.
+    
+    Args:
+        mbid (str): MBID for which similarity metrics should be computed.
+
+        offset (int, non-negative): submission offset indicating the
+        submission for the MBID whose metrics are being submitted.
+    """
     id = db.data.get_lowlevel_id(mbid, offset)
     submit_similarity_by_id(id)
 
